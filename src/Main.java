9b855caaf09ede97e5f00861f8d0c7f7c312a0db
Absolutt87 <pedrodriguez87@hotmail.com>
2018-10-05 09:06:23 +0200

    Commit java home work

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String go = repeatString("go", 3);
+        String xxx = repeatString("xxx", 0);
+        System.out.println(go);
+        System.out.println(xxx);
+
+        String go2 = padLeft("go", 5);
+        System.out.println(go2);
+
+        String go3 = padRight("go",5);
+        System.out.println(go3);
+
+        System.out.println(numbersRange(3,8));
+
+    }
+
+    /** This method Repeats a String n times */
+    private static String repeatString(String s, int number) {
+
+        String result = "";
+
+        for (int i = 1; i <= number; i++) {
+            result += s;
+        }
+        return result;
+    }
+
+    private static String padLeft(String s, int number) {
+
+        String result = "";
+        int lenght = s.length();
+
+        for (int i = 1; i <= (number - lenght); i++) {
+            result = result + " ";
+
+        }
+
+        return result + s;
+    }
+
+    private static String padRight(String s, int number) {
+
+        String result = "";
+        int lenght = s.length();
+
+        for (int j = 1; j < (number - lenght); j++) {
+            result = result + " ";
+
+        }
+
+        return s + result;
+    }
+
+    private static List<Integer> numbersRange (int a, int b) {
+
+        List<Integer> numbersRange = new ArrayList<>();
+        for (int i = a; i <= b; i++) {
+            numbersRange.add(i);
+
+        }
+        return numbersRange;
+    }
+
+}
